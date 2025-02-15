0d09926ea247b441e9a9b521c4429342966edb08
Lorena Muralha <mlmuralha2301@gmail.com>
2025-02-15 12:33:51 -0300

    feat: textura adicionado e ajuste de luz

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x87CEEB);
-const light = new THREE.DirectionalLight(0xffffff, 1.5);
-light.position.set(5, 10, 5);
+scene.background = new THREE.Color(0x87ceeb);
+const light = new THREE.DirectionalLight(0xffffff, 0.8);
+light.position.set(0, 5, 1);
 scene.add(light);
+const lightHelper = new THREE.DirectionalLightHelper(light, 1); // O segundo parâmetro é o tamanho do helper
+scene.add(lightHelper);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+light.castShadow = true;
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 500;
+
+// Carregar a textura para o road
+const textureLoader = new THREE.TextureLoader();
+const roadTexture = textureLoader.load("water-texture.jpg");
+roadTexture.wrapS = THREE.RepeatWrapping;
+roadTexture.wrapT = THREE.RepeatWrapping;
+roadTexture.repeat.set(4, 20); // Ajuste os valores de repeat conforme necessário
+
 const roadGeometry = new THREE.PlaneGeometry(10, 50);
-const roadMaterial = new THREE.MeshBasicMaterial({ color: 0x0D5DA8, side: THREE.DoubleSide });
+const roadMaterial = new THREE.MeshStandardMaterial({
+  map: roadTexture, // Aplicar a textura
+  side: THREE.DoubleSide,
+});
 const road = new THREE.Mesh(roadGeometry, roadMaterial);
 road.rotation.x = -Math.PI / 2;
 road.position.set(0, 0.1, -5);
 scene.add(road);
 
 const grassGeometry = new THREE.PlaneGeometry(100, 50);
-const grassMaterial = new THREE.MeshBasicMaterial({ color: 0x157E00, side: THREE.DoubleSide });
+const grassMaterial = new THREE.MeshBasicMaterial({
+  color: 0x157e00,
+  side: THREE.DoubleSide,
+});
 const grass = new THREE.Mesh(grassGeometry, grassMaterial);
 grass.rotation.x = -Math.PI / 2;
 grass.position.set(0, 0, -5);
@@ -28,15 +57,18 @@ scene.add(grass);
 
 const loader = new THREE.GLTFLoader();
 let plane;
-loader.load('objects/cartoon_plane.glb', (gltf) => {
-    plane = gltf.scene;
-    plane.position.set(0, 1, 0);
-    plane.scale.set(0.08, 0.08, 0.08);
-    plane.rotation.y = Math.PI;
-    scene.add(plane);
-    camera.lookAt(plane.position);
+loader.load("objects/cartoon_plane.glb", (gltf) => {
+  plane = gltf.scene;
+  plane.castShadow = true;
+  plane.position.set(0, 1, 0);
+  plane.scale.set(0.08, 0.08, 0.08);
+  plane.rotation.y = Math.PI;
+  scene.add(plane);
+  camera.lookAt(plane.position);
 });
 
+road.receiveShadow = true;
+grass.receiveShadow = true;
 camera.position.set(0, 3, 5);
 
 let planeSpeed = 0;
@@ -52,106 +84,116 @@ const maxAlpha = 3.0;
 const alphaIncrement = 0.01;
 
 function createObstacle() {
-    if (!plane) return;
-    alpha = Math.min(maxAlpha, alpha + alphaIncrement);
-    const numObstacles = Math.ceil(alpha * Math.random() * 3);
-    
-    for (let i = 0; i < numObstacles; i++) {
-        const loader = new THREE.GLTFLoader();
-        loader.load('objects/tnt.glb', (gltf) => {
-            const obstacle = gltf.scene;
-            obstacle.scale.set(0.05, 0.05, 0.05);
-            const offsetX = (Math.random() - 0.5) * 4 * alpha;
-            obstacle.position.set(plane.position.x + offsetX, 1, plane.position.z - 10);
-            scene.add(obstacle);
-            obstacles.push(obstacle);
-        });
-    }
+  if (!plane) return;
+  alpha = Math.min(maxAlpha, alpha + alphaIncrement);
+  const numObstacles = Math.ceil(alpha * Math.random() * 3);
+
+  for (let i = 0; i < numObstacles; i++) {
+    const loader = new THREE.GLTFLoader();
+    loader.load("objects/tnt.glb", (gltf) => {
+      const obstacle = gltf.scene;
+      obstacle.scale.set(0.05, 0.05, 0.05);
+      const offsetX = (Math.random() - 0.5) * 4 * alpha;
+      obstacle.position.set(
+        plane.position.x + offsetX,
+        1,
+        plane.position.z - 10
+      );
+      scene.add(obstacle);
+      obstacles.push(obstacle);
+    });
+  }
 }
 
 function createFuel() {
-    if (!plane) return;
-    const loader = new THREE.GLTFLoader();
-    loader.load('objects/fuel_tank.glb', (gltf) => {
-        const fuel = gltf.scene;
-        fuel.scale.set(0.01, 0.01, 0.01);
-        
-        let offsetX;
-        let validPosition = false;
-        while (!validPosition) {
-            offsetX = (Math.random() - 0.5) * 4 * alpha;
-            validPosition = !obstacles.some(obstacle => Math.abs(obstacle.position.x - offsetX) < 1);
-        }
-        
-        fuel.position.set(plane.position.x + offsetX, 1, plane.position.z - 10);
-        scene.add(fuel);
-        fuels.push(fuel);
-    });
+  if (!plane) return;
+  const loader = new THREE.GLTFLoader();
+  loader.load("objects/fuel_tank.glb", (gltf) => {
+    const fuel = gltf.scene;
+    fuel.scale.set(0.01, 0.01, 0.01);
+
+    let offsetX;
+    let validPosition = false;
+    while (!validPosition) {
+      offsetX = (Math.random() - 0.5) * 4 * alpha;
+      validPosition = !obstacles.some(
+        (obstacle) => Math.abs(obstacle.position.x - offsetX) < 1
+      );
+    }
+
+    fuel.position.set(plane.position.x + offsetX, 1, plane.position.z - 10);
+    scene.add(fuel);
+    fuels.push(fuel);
+  });
 }
 
 setInterval(createObstacle, 2000);
-// setInterval(createFuel, 5000);
+setInterval(createFuel, 5000);
 
 window.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowLeft") {
-        planeSpeed = -0.2;
-    } else if (event.key === "ArrowRight") {
-        planeSpeed = 0.2;
-    }
+  if (event.key === "ArrowLeft") {
+    planeSpeed = -0.2;
+  } else if (event.key === "ArrowRight") {
+    planeSpeed = 0.2;
+  }
 });
 
 window.addEventListener("keyup", () => {
-    planeSpeed = 0;
+  planeSpeed = 0;
 });
 
 function checkCollision(plane, object) {
-    return plane && Math.abs(plane.position.x - object.position.x) < 1 && Math.abs(plane.position.z - object.position.z) < 1.5;
+  return (
+    plane &&
+    Math.abs(plane.position.x - object.position.x) < 1 &&
+    Math.abs(plane.position.z - object.position.z) < 1.5
+  );
 }
 
 function animate() {
-    requestAnimationFrame(animate);
-    road.position.z += 0.1;
-    grass.position.z += 0.1;
-    if (road.position.z > 0) road.position.z = -5;
-    if (grass.position.z > 0) grass.position.z = -5;
+  requestAnimationFrame(animate);
+  road.position.z += 0.1;
+  grass.position.z += 0.1;
+  if (road.position.z > 0) road.position.z = -5;
+  if (grass.position.z > 0) grass.position.z = -5;
 
-    if (plane) {
-        plane.position.x += planeSpeed;
-        if (plane.position.x > roadLimit) plane.position.x = roadLimit;
-        if (plane.position.x < -roadLimit) plane.position.x = -roadLimit;
+  if (plane) {
+    plane.position.x += planeSpeed;
+    if (plane.position.x > roadLimit) plane.position.x = roadLimit;
+    if (plane.position.x < -roadLimit) plane.position.x = -roadLimit;
+  }
+
+  obstacles.forEach((obstacle, index) => {
+    obstacle.position.z += 0.1;
+    if (obstacle.position.z > 5) {
+      scene.remove(obstacle);
+      obstacles.splice(index, 1);
+    } else if (checkCollision(plane, obstacle)) {
+      scene.remove(obstacle);
+      obstacles.splice(index, 1);
+      lives--;
+      document.getElementById("lives").innerText = `Vidas: ${lives}`;
+      if (lives <= 0) {
+        alert("Game Over!");
+        location.reload();
+      }
     }
+  });
 
-    obstacles.forEach((obstacle, index) => {
-        obstacle.position.z += 0.1;
-        if (obstacle.position.z > 5) {
-            scene.remove(obstacle);
-            obstacles.splice(index, 1);
-        } else if (checkCollision(plane, obstacle)) {
-            scene.remove(obstacle);
-            obstacles.splice(index, 1);
-            lives--;
-            document.getElementById("lives").innerText = `Vidas: ${lives}`;
-            if (lives <= 0) {
-                alert("Game Over!");
-                location.reload();
-            }
-        }
-    });
+  fuels.forEach((fuel, index) => {
+    fuel.position.z += 0.1;
+    if (fuel.position.z > 5) {
+      scene.remove(fuel);
+      fuels.splice(index, 1);
+    } else if (checkCollision(plane, fuel)) {
+      scene.remove(fuel);
+      fuels.splice(index, 1);
+      lives++;
+      document.getElementById("lives").innerText = `Vidas: ${lives}`;
+    }
+  });
 
-    fuels.forEach((fuel, index) => {
-        fuel.position.z += 0.1;
-        if (fuel.position.z > 5) {
-            scene.remove(fuel);
-            fuels.splice(index, 1);
-        } else if (checkCollision(plane, fuel)) {
-            scene.remove(fuel);
-            fuels.splice(index, 1);
-            lives++;
-            document.getElementById("lives").innerText = `Vidas: ${lives}`;
-        }
-    });
-
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 animate();
